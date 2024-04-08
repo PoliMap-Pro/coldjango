9c78dc6395c096e5977f1d2b0cb53fe99529c743
andyucroo <andy@ucroo.com>
2024-04-09 00:58:49 +1000

    case1majority now prints,     Election on 2010-01-01         Seat of Frank                 Booth object (18)                         Party Hack, Australian Apathy Party, 3424                         Outmaneuvered Idealist, Independent, 424                         Dangerous Idealogue, World Tory Alliance, 123                         Narcissist, Independent, 45                 Booth object (21)                         Party Hack, Australian Apathy Party, 6848                         Outmaneuvered Idealist, Independent, 848                         Dangerous Idealogue, World Tory Alliance, 246                         Narcissist, Independent, 80
            Seat of Yossarian
                    Booth object (19)
                            Crypto-Fascist, World Tory Alliance, 456
                            Bored Billionaire, Independent, 77
                            Rural Blowhard, Independent, 162
                    Booth object (22)
                            Rural Blowhard, Independent, 841
    
    Election on 2013-01-01
            Seat of Frank
                    Booth object (18)
                            Outmaneuvered Idealist, Independent, 848
                            Scandal Magnet, Australian Apathy Party, 2345
                            Dangerous Idealogue, Independent, 3453
    
            Seat of Yossarian
                    Booth object (20)
                            Narcissist, World Tory Alliance, 1564
                            Bored Billionaire, Independent, 64
                            Rural Blowhard, Independent, 261
                    Booth object (22)
                            Narcissist, World Tory Alliance, 2018
                            Bored Billionaire, Independent, 18
                            Rural Blowhard, Independent, 683

--- mysite/jacobsladder/management/commands/case1majority.py
+++ mysite/jacobsladder/management/commands/case1majority.py
@@ -4,6 +4,47 @@ from ... import models
 
 
 class Command(BaseCommand):
+    """ Should print,
+    Election on 2010-01-01
+        Seat of Frank
+                Booth object (18)
+                        Party Hack, Australian Apathy Party, 3424
+                        Outmaneuvered Idealist, Independent, 424
+                        Dangerous Idealogue, World Tory Alliance, 123
+                        Narcissist, Independent, 45
+                Booth object (21)
+                        Party Hack, Australian Apathy Party, 6848
+                        Outmaneuvered Idealist, Independent, 848
+                        Dangerous Idealogue, World Tory Alliance, 246
+                        Narcissist, Independent, 80
+
+        Seat of Yossarian
+                Booth object (19)
+                        Crypto-Fascist, World Tory Alliance, 456
+                        Bored Billionaire, Independent, 77
+                        Rural Blowhard, Independent, 162
+                Booth object (22)
+                        Rural Blowhard, Independent, 841
+
+
+Election on 2013-01-01
+        Seat of Frank
+                Booth object (18)
+                        Outmaneuvered Idealist, Independent, 848
+                        Scandal Magnet, Australian Apathy Party, 2345
+                        Dangerous Idealogue, Independent, 3453
+
+        Seat of Yossarian
+                Booth object (20)
+                        Narcissist, World Tory Alliance, 1564
+                        Bored Billionaire, Independent, 64
+                        Rural Blowhard, Independent, 261
+                Booth object (22)
+                        Narcissist, World Tory Alliance, 2018
+                        Bored Billionaire, Independent, 18
+                        Rural Blowhard, Independent, 683
+    """
+
     help = 'Case 1'
 
     def handle(self, *arguments, **keywordarguments):
@@ -11,22 +52,22 @@ class Command(BaseCommand):
                 'election_date'):
             print(f"Election on {election.election_date}")
             for seat in election.seat_set.all():
-                print(f"Seat of {seat.name}")
-                for booth in seat.booth_set:
-                    print(booth)
-
-                #for voteTally in models.VoteTally.objects.filter(
-                #        seat=seat, election=election):
-                #    if models.Representation.objects.filter(
-                #            election=election,
-                #            person=voteTally.candidate.person).exists():
-                #        middle = models.Representation.objects.get(
-                #            election=election,
-                #            person=voteTally.candidate.person).party.name
-                #    else:
-                #        middle = "Independent"
-                #    print(f"{voteTally.candidate.person.name}, {middle}, "
-                #          f"{voteTally.primary_votes}")
+                print(f"\tSeat of {seat.name}")
+                for collection in models.Collection.objects.filter(
+                        seat=seat, election=election):
+                    print(f"\t\t{collection.booth}")
+                    for voteTally in models.VoteTally.objects.filter(
+                        booth=collection.booth, election=election):
+                        if models.Representation.objects.filter(
+                                election=election,
+                                person=voteTally.candidate.person).exists():
+                            middle = models.Representation.objects.get(
+                                election=election,
+                                person=voteTally.candidate.person).party.name
+                        else:
+                            middle = "Independent"
+                        print(f"\t\t\t{voteTally.candidate.person.name}, "
+                              f"{middle}, {voteTally.primary_votes}")
                 print()
             print()
 
