5db3bfcfb7da4475f11337d49a952a04eb84063a
andyucroo <andy@ucroo.com>
2024-04-10 15:13:58 +1000

    Wrote test case 3 from the email

--- mysite/jacobsladder/management/commands/case3prime.py
+++ mysite/jacobsladder/management/commands/case3prime.py
@@ -11,9 +11,20 @@ class Command(BaseCommand):
         twenty_ten = datetime(year=2010, month=1, day=1)
         house_election_2010 = models.HouseElection.objects.get(election_date=twenty_ten)
         seat1 = models.Seat.objects.get(name="Frank", state=models.StateName.VIC)
-        if models.Booth.objects.all().count() < 5:
-            booth5 = models.Booth()
-            booth5.save()
-            collection5 = models.Collection(booth=booth5, seat=seat1,
-                                            election=house_election_2010)
-            collection5.save()
+
+        models.Booth.per(Command.show_major_parties_primary)(seat1, house_election_2010)
+
+    @staticmethod
+    def show_major_parties_primary(election, seat, booth):
+        print(booth)
+        models.VoteTally.per(Command.show_primary_if_major)(booth, seat, election)
+
+    @staticmethod
+    def show_primary_if_major(election, seat, booth, vote_tally):
+        if models.Representation.objects.filter(person=vote_tally.candidate.person,
+                                                election=election).exists():
+            abbreviation = models.Representation.objects.get(
+                person=vote_tally.candidate.person,
+                election=election).party.abbreviation
+            if abbreviation in ('AAP', 'WTA', ):
+                print(f"{abbreviation}: {vote_tally.primary_votes}")
